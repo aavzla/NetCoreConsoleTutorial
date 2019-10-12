5b194da7b427b978c3ccb3faf2f19c32ac1765bc
aavzla <avilan.andres.cem@gmail.com>
2019-10-12 09:25:05 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreConsoleTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
